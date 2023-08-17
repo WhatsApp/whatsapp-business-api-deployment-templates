c51b981bc4b683fc8cad29b267ea99d6609c91ca
shakeeb80 <37278621+shakeeb80@users.noreply.github.com>
2023-08-17 11:49:29 +0000

    [MySQL 8] Update gcp deployement to use mysql8 database

--- src/gcp/k8s-db.tf
+++ src/gcp/k8s-db.tf
@@ -85,7 +85,7 @@ resource "kubernetes_stateful_set" "db" {
 
         container {
           name              = "db-server"
-          image             = "mysql:5.7.35"                                                                                |            image             = ""
+          image             = "mysql:8.0.34"                                                                                |            image             = ""
           image_pull_policy = "IfNotPresent"
 
 
